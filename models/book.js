4ebb102d532d593ca8bf967b79ab6790d7110813
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-11-22 22:08:55 -0700

    Changed book schema

--- models/book.js
+++ models/book.js
@@ -4,8 +4,9 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
+  thumbnail: { type: String, default: ""},
   synopsis: String,
-  date: { type: Date, default: Date.now }
+  publishedDate: { type: Date, default: Date.now }
 });
 
 const Book = mongoose.model("Book", bookSchema);
