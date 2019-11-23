b6be71e7ffb9f1664d2863449d1ede17972839f1
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-11-22 22:10:09 -0700

    Added a book component

--- client/src/components/Book/index.js
+++ client/src/components/Book/index.js
@@ -3,23 +3,24 @@ import SaveBtn from "../../components/SaveBtn";
 import DeleteBtn from "../../components/DeleteBtn";
 import "./style.css";
 
-function Book({ id, title, authors, publishedDate, thumbnail, saveBook }) {
+function Book({ id, title, authors, publishedDate, description, thumbnail, saveBookClick }) {
     return (
         <div className="row" key={id} >
             <div className="col s12 m6">
-                <div className="card">
+                <div className="card large">
                     <div className="card-image">
-                        <img alt="Book" src={thumbnail} className="img-fluid" />
-                        <span className="card-title">{title}</span>
+                        <img alt="Book" src={thumbnail} />
+                        
                     </div>
                     <div className="card-content">
+                    <span className="card-title grey-text text-darken-2">{title}</span>
                         <p>by {authors}</p>
                         <p>Published - {publishedDate}</p>
-
+                        <p className="grey-text text-darken-1">{description}</p>
                     </div>
                 
                 <div class="card-action">
-                    <SaveBtn id={id} onClick={saveBook} />
+                    <SaveBtn id={id} onClick={saveBookClick} />
                 </div>
             </div>
         </div>
